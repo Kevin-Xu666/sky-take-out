2ab22799ca8583392ce185b640270728be6c176b
xuzesen <1335718052@qq.com>
2024-10-09 15:41:25 +0800

    day02新增员工，解决相同员工插入，Threadlocal使用

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,26 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 处理sql异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //添加相同员工
+        //报错：Duplicate entry 'zhangsan' for key 'employee.idx_username'
+        String message = ex.getMessage(); //获得报错信息
+        if (message.contains("Duplicate entry")){
+            String[] split = message.split(" "); //对报错信息分割
+            String username = split[2]; //信息中第三个字符串就是重复的用户名
+            String msg = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        } else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
